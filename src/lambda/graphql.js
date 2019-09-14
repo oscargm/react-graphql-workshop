e4f0ea48b21b2e42bc89b6cb16cfe07434413292
Glenn Reyes <glenn@glennreyes.com>
2019-09-14 12:45:25 +0200

    Umcomment context

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -7,19 +7,19 @@ import typeDefs from './graphql/schema';
 const server = new ApolloServer({
   resolvers,
   typeDefs,
-  // context: async ({ req }) => {
-  //   const token = req.headers.authorization
-  //     ? req.headers.authorization.replace('Bearer ', '')
-  //     : null;
+  context: async ({ req }) => {
+    const token = req.headers.authorization
+      ? req.headers.authorization.replace('Bearer ', '')
+      : null;
 
-  //   if (token) {
-  //     const payload = await util.promisify(jwt.verify)(token, 'whateversecret');
+    if (token) {
+      const payload = await util.promisify(jwt.verify)(token, 'whateversecret');
 
-  //     return { user: payload.user };
-  //   }
+      return { user: payload.user };
+    }
 
-  //   return null;
-  // },
+    return null;
+  },
   introspection: true,
   playground: true,
 });
